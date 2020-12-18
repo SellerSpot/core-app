c7ce5be29aab98314c22cadb949d58344c075dcc
ThayalanGR <grthayalan18@gmail.com>
2020-12-18 10:16:53 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # react-wept-starter
+
 React typescript starter with prettier and eslint support built with webapack (not cra template / cra ts template)
+
+```
+            Typescript Prettier eslint react app starter
+
+            no cra template => completely built with webpack
+
+            features:-
+                + react
+                + webpack
+                + typescript
+                + prettier
+                + eslint
+
+            supported file extensions:-
+                + ts
+                + tsx
+                + js
+                + jsx
+                + css
+
+            Note: for additional file extensions, install corresponding loaders.
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
 import React, {ReactElement} from 'react';
 
 export const App = (): ReactElement => {
-    return (
-        <div>
-            Typescript Prettier eslint react app starter
-            <br />
-            <br />
-            no cra template =&gt; completely built with webpack
-            <br />
-            <br />
-            supported file extensions:-
-            <br />
-            <ul>
-                <li>ts</li>
-                <li>tsx</li>
-                <li>js</li>
-                <li>jsx</li>
-                <li>css</li>
-            </ul>
-            <br />
-            Note: for additional file extension install corresponding loaders
-        </div>
-    );
+    return <div>Typescript Prettier eslint react app starter</div>;
 };
