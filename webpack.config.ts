3d0be8e6211f4a2448c3580d5c4abada250829e7
ThayalanGR <grthayalan18@gmail.com>
2020-12-19 16:03:08 +0530

    configuration tweaks

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,51 +5,58 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import packageJson from './package.json';
 
-const webpackConfiguration = (env): Configuration => ({
-    entry: './src',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        plugins: [new TsconfigPathsPlugin()],
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'build.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                options: {
-                    transpileOnly: true,
+const webpackConfiguration = (env: {
+    production: boolean;
+    development: boolean;
+}): Configuration => {
+    const isProduction = env.production ? true : false;
+    return {
+        entry: 'src',
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+            plugins: [new TsconfigPathsPlugin()],
+        },
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'index.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                    exclude: [/dist/, /node_modules/],
                 },
-                exclude: /dist/,
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, '/public/index.html'),
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': isProduction ? 'production' : 'development',
+                'process.env.APP_NAME': JSON.stringify(packageJson.name),
+                'process.env.APP_VERSION': JSON.stringify(packageJson.version),
+            }),
+            new ForkTsCheckerWebpackPlugin({
+                eslint: {
+                    files: 'src',
+                },
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, '/public/index.html'),
-        }),
-        new webpack.DefinePlugin({
-            'process.env.PRODUCTION': env.production ?? false,
-            'process.env.NAME': packageJson.name,
-            'process.env.VERSION': packageJson.version,
-        }),
-        new ForkTsCheckerWebpackPlugin({
-            eslint: {
-                files: 'src',
-            },
-        }),
-    ],
-    devServer: {
-        publicPath: '/',
-        port: 8000,
-    },
-});
+        devServer: {
+            publicPath: '/',
+            port: 8000,
+        },
+        devtool: !isProduction ? 'source-map' : false,
+    };
+};
 
 export default webpackConfiguration;
