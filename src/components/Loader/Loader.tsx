16b87c0aa947a532bdc7ae8f606d31cbe2ea0179
Rohit Raj <rij7u7d@gmail.com>
2021-03-25 21:00:37 +0530

    Created DummyComponent and got controls to work for its storybook

--- src/components/Loader/Loader.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactElement } from 'react';
-import styles from './loader.module.scss';
-
-export const Loader = (): ReactElement => {
-    return <div className={styles.loaderWrapper}>Please wait, loading..</div>;
-};
