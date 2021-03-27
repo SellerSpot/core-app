1cd3b414cd96747fb7352a5081c7b96f5b74c493
Rohit Raj <rij7u7d@gmail.com>
2021-03-27 20:14:40 +0530

    Setup welcome component and added theaming to storybook

---
+++ src/components/WelcomeComponent.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import ThemeProvider from 'components/ThemeSetter/ThemeProvider';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
+
+interface IWelcomeComponentProps {
+    message: string;
+}
+
+const Template: Story<IWelcomeComponentProps> = (args: IWelcomeComponentProps) => (
+    <Provider store={store}>
+        <ThemeProvider>
+            <h1>{args.message}</h1>
+        </ThemeProvider>
+    </Provider>
+);
+
+export const WelcomeComponent = Template.bind({});
+WelcomeComponent.args = {
+    message: 'Welcome to Core-App Storybook',
+} as IWelcomeComponentProps;
+
+export default {
+    title: 'Components',
+    component: WelcomeComponent,
+} as Meta;
