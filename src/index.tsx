e811a2de8dca6a1bb19d79eb3304d811c67668f2
ThayalanGR <grthayalan18@gmail.com>
2021-03-27 16:05:24 +0530

    themeprovider moved to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'store/store';
 import { App } from 'layouts/App/App';
+import ThemeProvider from 'components/ThemeSetter/ThemeProvider';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
