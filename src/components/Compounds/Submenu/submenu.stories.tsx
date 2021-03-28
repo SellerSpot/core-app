462ff23e3bd15295617b5a9d39be12ee48cd7e96
Rohit Raj <rij7u7d@gmail.com>
2021-03-28 22:00:25 +0530

    components/Compounds/Submenu/submenu.stories: Setup component in storybook

--- src/components/Compounds/Submenu/submenu.stories.tsx
+++ src/components/Compounds/Submenu/submenu.stories.tsx
@@ -1,33 +1,68 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import ThemeProvider from 'components/ThemeSetter/ThemeProvider';
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'store/store';
 import Submenu from './Submenu';
 import { ISubmenuProps } from './Submenu.types';
 
-const Template: Story<ISubmenuProps> = (args: ISubmenuProps) => (
-    <BrowserRouter>
-        <Provider store={store}>
-            <ThemeProvider>
-                <div
-                    style={{
-                        padding: 0,
-                        margin: 0,
-                        width: '100%',
-                        height: '100vh',
-                    }}
-                >
-                    <Submenu {...args} />
-                </div>
-            </ThemeProvider>
-        </Provider>
-    </BrowserRouter>
-);
+// demo functional component to help demo the SubMenu component (since it needs state)
+function DemoLoader() {
+    // storing the tiles in localstate
+    const [tilesState, setTilesState] = useState<ISubmenuProps['tiles']>([
+        {
+            title: 'Sample',
+            childTilesVisible: false,
+            disabled: true,
+            pathToWatch: [''],
+            childTiles: [
+                {
+                    title: 'SubMenu',
+                    pathToWatch: [''],
+                    disabled: false,
+                },
+            ],
+        },
+        {
+            title: 'Sample',
+            childTilesVisible: true,
+            disabled: false,
+            pathToWatch: ['/iframe.html'],
+            childTiles: [
+                {
+                    title: 'SubMenu',
+                    disabled: false,
+                    pathToWatch: ['/iframe.html'],
+                },
+            ],
+        },
+    ]);
+
+    return (
+        <BrowserRouter>
+            <Provider store={store}>
+                <ThemeProvider>
+                    <div
+                        style={{
+                            padding: 0,
+                            margin: 0,
+                            width: '100%',
+                            height: '100vh',
+                        }}
+                    >
+                        <Submenu tiles={tilesState} />
+                    </div>
+                </ThemeProvider>
+            </Provider>
+        </BrowserRouter>
+    );
+}
+
+const Template: Story<ISubmenuProps> = (args: ISubmenuProps) => DemoLoader();
 
 export const SubmenuComponent = Template.bind({});
-SubmenuComponent.args = {} as ISubmenuProps;
+SubmenuComponent.args = {};
 
 export default {
     title: 'Components/Compounds',
