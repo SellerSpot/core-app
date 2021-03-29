9cbcb5cf1308afad1ba0f120398666d35fd02948
Rohit Raj <rij7u7d@gmail.com>
2021-03-30 03:44:11 +0530

    src: Implemented TradeMark component

--- src/components/Compounds/WorkSpaceMenu/WorkSpaceMenu.tsx
+++ src/components/Compounds/WorkSpaceMenu/WorkSpaceMenu.tsx
@@ -1,13 +1,24 @@
+import cn from 'classnames';
 import Trademark from 'components/Atoms/Trademark/Trademark';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './WorkSpaceMenu.module.scss';
 
 export default function WorkSpaceMenu() {
+    const [userHovering, setUserHovering] = useState(false);
+
     return (
-        <div className={styles.wrapper}>
+        <div
+            className={cn(styles.wrapper, { [styles.wrapperExpanded]: userHovering })}
+            onMouseOver={() => setUserHovering(true)}
+            onMouseLeave={() => setUserHovering(false)}
+        >
             <div className={styles.storeInformationWrapper}></div>
             <div className={styles.workspacesWrapper}></div>
-            <div className={styles.trademarkWrapper}>
+            <div
+                className={cn(styles.trademarkWrapper, {
+                    [styles.trademarkWrapperExpanded]: userHovering,
+                })}
+            >
                 <Trademark />
             </div>
         </div>
