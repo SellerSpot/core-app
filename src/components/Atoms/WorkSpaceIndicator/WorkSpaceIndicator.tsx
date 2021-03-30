9215f0cf78e5fa428ff66faee5e00045e7629639
Rohit Raj <rij7u7d@gmail.com>
2021-03-30 05:44:49 +0530

    components: Modified WorkSpaceTile to work for WorkspaceIndicator

--- src/components/Atoms/WorkSpaceIndicator/WorkSpaceIndicator.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { IWorkSpaceIndicatorProps } from './WorkSpaceIndicator.types';
-
-export default function WorkSpaceIndicator(props: IWorkSpaceIndicatorProps) {
-    return <div>WorkSpaceIndicator</div>;
-}
