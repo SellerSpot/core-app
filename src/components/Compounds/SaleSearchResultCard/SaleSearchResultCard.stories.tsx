4393c8130e69e7ab32b42b29f099a9bbc59c3979
Rohit Raj <rij7u7d@gmail.com>
2021-04-09 15:24:01 +0530

    components: Completed SaleSearchCard Component

---
+++ src/components/Compounds/SaleSearchResultCard/SaleSearchResultCard.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import ThemeProvider from 'components/ThemeProvider/ThemeProvider';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
+import SaleSearchResultCardComponent from './SaleSearchResultCard';
+import { ISaleSearchResultCard } from './SaleSearchResultCard.types';
+
+const Template: Story = (args: ISaleSearchResultCard) => (
+    <Provider store={store}>
+        <ThemeProvider>
+            <SaleSearchResultCardComponent {...args} />
+        </ThemeProvider>
+    </Provider>
+);
+export const SaleSearchResultCard = Template.bind({});
+SaleSearchResultCard.args = {
+    productName: 'Tomatoes',
+    stockUnit: 'Kg',
+    unitPrice: 200,
+    productImage: 'https://i.ibb.co/MZMJMCn/Rectangle-18.png',
+} as ISaleSearchResultCard;
+export default {
+    title: 'Components/Compounds',
+    component: SaleSearchResultCardComponent,
+} as Meta;
---
+++ src/components/Compounds/SaleSearchResultCard/SaleSearchResultCard.tsx
@@ -0,0 +1,35 @@
+import Card from 'components/Atoms/Card/Card';
+import React, { ReactElement } from 'react';
+import { ISaleSearchResultCard } from './SaleSearchResultCard.types';
+import styles from './SaleSearchResultCard.module.scss';
+import { numberFormatINRCurrency } from 'utilities/general';
+import { isUndefined } from 'lodash';
+
+export default function SaleSearchResultCard(props: ISaleSearchResultCard): ReactElement {
+    return (
+        <Card
+            className={{
+                cardWrapper: styles.cardWrapper,
+                contentWrapper: styles.contentWrapper,
+            }}
+            onClickCard={props.onClick}
+            content={
+                <div className={styles.content}>
+                    <div className={styles.contentLHS}>
+                        {isUndefined(props.productImage) ? null : (
+                            <div className={styles.image}>
+                                <img className={styles.image} src={props.productImage} />
+                            </div>
+                        )}
+                        <div>
+                            <h6 className={styles.title}>{props.productName}</h6>
+                        </div>
+                    </div>
+                    <div className={styles.contentRHS}>
+                        <h6>{`${numberFormatINRCurrency(props.unitPrice)}/${props.stockUnit}`}</h6>
+                    </div>
+                </div>
+            }
+        />
+    );
+}
