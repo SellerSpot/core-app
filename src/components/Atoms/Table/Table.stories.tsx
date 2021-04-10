9bd2491af3b7a377c6b6d0c3608e4fab23e876a3
Rohit Raj <rij7u7d@gmail.com>
2021-04-10 16:21:42 +0530

    Midway implementing the Table Component

---
+++ src/components/Atoms/Table/Table.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import ThemeProvider from 'components/ThemeProvider/ThemeProvider';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
+import TableComponent from './Table';
+import { ITableProps } from './Table.types';
+
+const Template: Story<ITableProps> = (args: ITableProps) => (
+    <Provider store={store}>
+        <ThemeProvider>
+            <TableComponent {...args} />
+        </ThemeProvider>
+    </Provider>
+);
+
+export const Table = Template.bind({});
+Table.args = {
+    headers: [
+        {
+            content: 'Sno',
+        },
+        {
+            content: 'Name',
+        },
+        {
+            content: 'Password',
+        },
+    ],
+    rowData: [
+        [
+            {
+                content: '1',
+            },
+            {
+                content: 'Rohit',
+            },
+            {
+                content: 'passwordstring',
+            },
+        ],
+    ],
+} as ITableProps;
+
+export default {
+    title: 'Components/Atoms',
+    component: TableComponent,
+} as Meta;
