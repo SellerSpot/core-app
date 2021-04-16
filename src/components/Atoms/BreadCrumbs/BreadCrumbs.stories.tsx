8c9eee2795330ea66b716b530fee0e21734eb266
Rohit Raj <rij7u7d@gmail.com>
2021-04-16 11:21:07 +0530

    Added version bump workflow and script

--- src/components/Atoms/BreadCrumbs/BreadCrumbs.stories.tsx
+++ src/components/Atoms/BreadCrumbs/BreadCrumbs.stories.tsx
@@ -1,9 +1,6 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
-import ThemeProvider from 'components/ThemeProvider/ThemeProvider';
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from 'store/store';
 import BreadCrumbsComponent from './BreadCrumbs';
 
 const Template: Story = () => (
