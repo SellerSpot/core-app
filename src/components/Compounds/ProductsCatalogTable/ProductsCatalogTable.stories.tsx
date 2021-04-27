0eb49ceb1fd4a07745f18028e82e287516607ae9
Rohit Raj <rij7u7d@gmail.com>
2021-04-27 10:51:48 +0530

    Completed ProductsCatalogDetails

---
+++ src/components/Compounds/ProductsCatalogTable/ProductsCatalogTable.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import { numberFormatINRCurrency } from 'utilities/general';
+import {
+    ProductsCatalogTable as ProductsCatalogTableComponent,
+    IProductsCatalogTableProps,
+} from './ProductsCatalogTable';
+
+const Template: Story<IProductsCatalogTableProps> = (args: IProductsCatalogTableProps) => (
+    <ProductsCatalogTableComponent {...args} />
+);
+
+export const ProductsCatalogTable = Template.bind({});
+ProductsCatalogTable.args = {
+    products: [
+        {
+            active: true,
+            productName: 'Sample Product',
+            brand: 'Brand',
+            category: 'Category',
+            description:
+                'This is demo of a super long description of the product which we are goona use to describe the product to anyone who uses this software',
+            availableStock: '20 pcs',
+            sellingPrice: numberFormatINRCurrency(234),
+        },
+    ],
+} as IProductsCatalogTableProps;
+
+export default {
+    title: 'Components',
+    component: ProductsCatalogTableComponent,
+} as Meta;
