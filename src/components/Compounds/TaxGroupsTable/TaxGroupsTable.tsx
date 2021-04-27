fae0f86fd5ed0877e987e1a6d6f627726312d5de
Rohit Raj <rij7u7d@gmail.com>
2021-04-27 19:20:39 +0530

    Added TaxBracketsGrouped component

---
+++ src/components/Compounds/TaxGroupsTable/TaxGroupsTable.tsx
@@ -0,0 +1,47 @@
+import { ITableProps, ITableRow, Table } from '@sellerspot/universal-components';
+import React, { ReactElement } from 'react';
+import { TaxBracketsGrouped } from './Components/TaxBracketsGrouped';
+import { TaxGroupsTableService } from './TaxGroupsTable.service';
+import { ITaxGroupsTableProps } from './TaxGroupsTable.types';
+
+export { ITaxGroupsTableProps } from './TaxGroupsTable.types';
+
+// assembles the body content for the table
+const getTableBody = (props: {
+    tableItems: ITaxGroupsTableProps['tableItems'];
+    toggleRowExpansion: (rowIndex: number) => void;
+}): ITableRow[] => {
+    const { tableItems, toggleRowExpansion } = props;
+    return tableItems.map(
+        (tableItem, tableItemIndex): ITableRow => {
+            const { brackets } = tableItem;
+            return {
+                cells: TaxGroupsTableService.tableCells({
+                    tableItem,
+                }),
+                onClick: () => toggleRowExpansion(tableItemIndex),
+                collapsedContent: <TaxBracketsGrouped brackets={brackets} />,
+            };
+        },
+    );
+};
+
+export const TaxGroupsTable = (props: ITaxGroupsTableProps): ReactElement => {
+    const { tableItems } = props;
+
+    const tableBody: ITableProps['body'] = ({ toggleRowExpansion }) => {
+        return getTableBody({
+            tableItems,
+            toggleRowExpansion,
+        });
+    };
+
+    return (
+        <Table
+            stickyHeader
+            hasExpandableRows
+            headers={TaxGroupsTableService.tableHeaders}
+            body={tableBody}
+        />
+    );
+};
