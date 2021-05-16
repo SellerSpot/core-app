77f09f451101a8ece59e4bd1b82d253e59005f89
ThayalanGR <grthayalan18@gmail.com>
2021-05-16 13:28:07 +0530

    minor file restructruing

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -1,16 +1,19 @@
 import { Route, Switch } from 'react-router-dom';
 import React, { ReactElement } from 'react';
+
+import '../../styles/core.scss';
+import styles from './app.module.scss';
+
 import { Dashboard } from 'layouts/Dashboard/Dashboard';
 import { ROUTES } from 'config/routes';
 import { initializeGlobalConfig } from 'config/globalConfig';
-import styles from './app.module.scss';
 import { CommonProvider } from './components/CommonProvider/CommonProvider';
 import ThemeProvider from './components/ThemeProvider/ThemeProvider';
 import TenantProvider from './components/TenantProvider/TenantProvider';
 import AuthProvider from './components/AuthProvider/AuthProvider';
 
 // global actions
-initializeGlobalConfig(); // application common initilizers goes here
+initializeGlobalConfig();
 
 export const App = (): ReactElement => {
     return (
