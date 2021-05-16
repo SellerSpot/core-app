2f92371af9f092374cd11f653f17382b3acc024d
ThayalanGR <grthayalan18@gmail.com>
2021-05-16 15:00:46 +0530

    storybook issue fix

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import { Dialog, Notify, ThemeProvider } from '@sellerspot/universal-components';
 import React, { ReactElement } from 'react';
 import { Provider } from 'react-redux';
-import { CommonProvider } from '../src/components/CommonProvider/CommonProvider';
+import { CommonProvider } from '../src/layouts/App/components/CommonProvider/CommonProvider';
 import { colorThemes, fontSizeThemes } from '../src/config/themes';
 import { store } from '../src/store/store';
 import 'react-sortable-tree/style.css';
