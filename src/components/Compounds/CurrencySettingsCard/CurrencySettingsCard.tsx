72d16ea03029d6928e90ef417cebea9616be8152
Rohit Raj <rij7u7d@gmail.com>
2021-06-13 20:08:37 +0530

    Temp fix on Select Component

--- src/components/Compounds/CurrencySettingsCard/CurrencySettingsCard.tsx
+++ src/components/Compounds/CurrencySettingsCard/CurrencySettingsCard.tsx
@@ -1,36 +1,36 @@
-import React, { ReactElement, useState } from 'react';
-import { Card, Select } from '@sellerspot/universal-components';
-import { ICurrencySettingsCardProps } from './CurrencySettingsCard.types';
-import styles from './CurrencySettingsCard.module.scss';
+// import React, { ReactElement, useState } from 'react';
+// import { Card } from '@sellerspot/universal-components';
+// import { ICurrencySettingsCardProps } from './CurrencySettingsCard.types';
+// import styles from './CurrencySettingsCard.module.scss';
 
-export default function CurrencySettingsCard(props: ICurrencySettingsCardProps): ReactElement {
-    const [selectedCurrency, setSelectedCurrency] = useState(0);
-    return (
-        <Card
-            className={{
-                cardWrapper: styles.cardWrapper,
-                contentWrapper: styles.contentWrapper,
-            }}
-            content={
-                <div className={styles.content}>
-                    <div className={styles.contentLHS}>
-                        <h5>Store Currency</h5>
-                        <p>Please select the currency to use across your store</p>
-                    </div>
-                    <Select
-                        options={props.currencies.map((currency, index) => {
-                            return {
-                                text: `${currency.currencyName} (${currency.currencyLogo})`,
-                                value: index,
-                            };
-                        })}
-                        onChange={(event) => {
-                            setSelectedCurrency(+event.target.value);
-                        }}
-                        value={selectedCurrency}
-                    />
-                </div>
-            }
-        />
-    );
-}
+// export default function CurrencySettingsCard(props: ICurrencySettingsCardProps): ReactElement {
+//     const [selectedCurrency, setSelectedCurrency] = useState(0);
+//     return (
+//         <Card
+//             className={{
+//                 cardWrapper: styles.cardWrapper,
+//                 contentWrapper: styles.contentWrapper,
+//             }}
+//             content={
+//                 <div className={styles.content}>
+//                     <div className={styles.contentLHS}>
+//                         <h5>Store Currency</h5>
+//                         <p>Please select the currency to use across your store</p>
+//                     </div>
+//                     {/* <Select
+//                         options={props.currencies.map((currency, index) => {
+//                             return {
+//                                 text: `${currency.currencyName} (${currency.currencyLogo})`,
+//                                 value: index,
+//                             };
+//                         })}
+//                         onChange={(event) => {
+//                             setSelectedCurrency(+event.target.value);
+//                         }}
+//                         value={selectedCurrency}
+//                     /> */}
+//                 </div>
+//             }
+//         />
+//     );
+// }
