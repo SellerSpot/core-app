899a2bc67111ad67a1e1b998714676b427232058
ThayalanGR <grthayalan18@gmail.com>
2021-08-04 07:06:59 +0530

    saleshistory expanded view additional information integration done

--- src/pages/PointOfSale/SalesHistory/components/SaleHistoryExpandedView/SaleHistoryExpandedView.tsx
+++ src/pages/PointOfSale/SalesHistory/components/SaleHistoryExpandedView/SaleHistoryExpandedView.tsx
@@ -13,9 +13,29 @@ export const SaleHistoryExpandedView = (props: { rowData: ISaleData }): ReactEle
     const {
         rowData: {
             cart,
-            payment: { totalDiscount, totalTax, amountPaid, grandTotal, balanceGiven, subTotal },
+            payment: {
+                totalDiscount,
+                totalTax,
+                amountPaid,
+                grandTotal,
+                balanceGiven,
+                subTotal,
+                method: paymentMethod,
+            },
+            customer: { name: customerName },
         },
     } = props;
+
+    // handlers
+    const onEditSaleClickHandler = () => {
+        // do onEditSaleClickHandler action
+    };
+    const onPrintReceiptClickHandler = () => {
+        // do onPrintReceiptClickHandler action
+    };
+    const onVoidSaleClickHandler = () => {
+        // do onVoidSaleClickHandler action
+    };
 
     // table render helpers
     const serialNumberRenderer: TTableCellCustomRenderer<ICartDetails> = (props) => {
@@ -98,32 +118,47 @@ export const SaleHistoryExpandedView = (props: { rowData: ISaleData }): ReactEle
                     <div className={styles.tableHolder}>
                         <Table {...tableProps} />
                     </div>
-                    <div className={styles.saleSummaryHolder}>
-                        <div className={styles.saleSummaryGroup}>
-                            <div>Sub-total</div>
-                            <div>{subTotal}</div>
+                    <div className={styles.summaryWrapper}>
+                        <div className={styles.secitonWrapper}>
+                            <h6>Customer details</h6>
+                            <div className={styles.customerDetailsHolder}>
+                                {
+                                    customerName
+                                    // give view more link and fetch customer details on demand on view more click and append in the same dom
+                                }
+                            </div>
                         </div>
-                        <div className={styles.saleSummaryGroup}>
-                            <div>Total tax</div>
-                            <div>{totalTax}</div>
+                        <div className={styles.secitonWrapper}>
+                            <h6>Payment method</h6>
+                            <div className={styles.paymentCard}>{paymentMethod}</div>
                         </div>
-                        <div className={styles.saleSummaryGroup}>
-                            <div>Total discount</div>
-                            <div>{totalDiscount}</div>
-                        </div>
-                        <div className={styles.summaryGroupHorizontalRule} />
-                        <div className={styles.saleSummaryGroup}>
-                            <h5>Grand total</h5>
-                            <h5>{grandTotal}</h5>
-                        </div>
-                        <div className={styles.summaryGroupHorizontalRule} />
-                        <div className={styles.saleSummaryGroup}>
-                            <div>Amount paid</div>
-                            <div>{amountPaid}</div>
-                        </div>
-                        <div className={styles.saleSummaryGroup}>
-                            <div>Balance</div>
-                            <div>{balanceGiven}</div>
+                        <div className={styles.salesTotalHolder}>
+                            <div className={styles.saleSummaryGroup}>
+                                <div>Sub-total</div>
+                                <div>{subTotal}</div>
+                            </div>
+                            <div className={styles.saleSummaryGroup}>
+                                <div>Total tax</div>
+                                <div>{totalTax}</div>
+                            </div>
+                            <div className={styles.saleSummaryGroup}>
+                                <div>Total discount</div>
+                                <div>{totalDiscount}</div>
+                            </div>
+                            <div className={styles.summaryGroupHorizontalRule} />
+                            <div className={styles.saleSummaryGroup}>
+                                <h6>Grand total</h6>
+                                <h6>{grandTotal}</h6>
+                            </div>
+                            <div className={styles.summaryGroupHorizontalRule} />
+                            <div className={styles.saleSummaryGroup}>
+                                <div>Amount paid</div>
+                                <div>{amountPaid}</div>
+                            </div>
+                            <div className={styles.saleSummaryGroup}>
+                                <div>Balance</div>
+                                <div>{balanceGiven}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -136,14 +171,22 @@ export const SaleHistoryExpandedView = (props: { rowData: ISaleData }): ReactEle
                         theme="primary"
                         label="Edit Sale"
                         fullWidth={true}
+                        onClick={onEditSaleClickHandler}
                     />
                     <Button
                         variant="outlined"
                         theme="primary"
                         label="Print Receipt"
                         fullWidth={true}
+                        onClick={onPrintReceiptClickHandler}
                     />
-                    <Button variant="outlined" theme="danger" label="Void Sale" fullWidth={true} />
+                    <Button
+                        variant="outlined"
+                        theme="danger"
+                        label="Void Sale"
+                        fullWidth={true}
+                        onClick={onVoidSaleClickHandler}
+                    />
                 </div>
             </div>
         </div>
