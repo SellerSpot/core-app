3e8688464897809d7787d2f342ada5a7375f31a9
ThayalanGR <grthayalan18@gmail.com>
2021-08-16 07:02:06 +0530

    number formatting and billa4 spacing issues fix

--- src/components/Compounds/Bill90MM/Components/Bill90MMSummary/Bill90MMSummary.tsx
+++ src/components/Compounds/Bill90MM/Components/Bill90MMSummary/Bill90MMSummary.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import { IBill90MMChildProps } from '../../Bill90MM.types';
 import styles from './Bill90MMSummary.module.scss';
 import mainStyles from '../../Bill90MM.module.scss';
+import { numberFormatINRCurrency } from '@sellerspot/universal-components';
 
 export const Bill90MMSummary = (props: IBill90MMChildProps): ReactElement => {
     const {
@@ -17,25 +18,27 @@ export const Bill90MMSummary = (props: IBill90MMChildProps): ReactElement => {
             <div className={styles.saleSummary}>
                 <div className={styles.summaryRow}>
                     <p className={styles.summaryRowTitle}>SUB-TOTAL</p>
-                    <p className={styles.summaryRowValue}>{subTotal}</p>
+                    <p className={styles.summaryRowValue}>{numberFormatINRCurrency(subTotal)}</p>
                 </div>
             </div>
             <div className={styles.saleSummary}>
                 <div className={styles.summaryRow}>
                     <p className={styles.summaryRowTitle}>DISCOUNT (sale)</p>
-                    <p className={styles.summaryRowValue}>{totalDiscount}</p>
+                    <p className={styles.summaryRowValue}>
+                        {numberFormatINRCurrency(totalDiscount)}
+                    </p>
                 </div>
             </div>
             <div className={styles.saleSummary}>
                 <div className={styles.summaryRow}>
                     <p className={styles.summaryRowTitle}>{`TAX (${grandTotalTaxPercentage})`}</p>
-                    <p className={styles.summaryRowValue}>{totalTax}</p>
+                    <p className={styles.summaryRowValue}>{numberFormatINRCurrency(totalTax)}</p>
                 </div>
             </div>
             <div className={styles.saleSummary}>
                 <div className={styles.summaryRow}>
                     <p className={styles.summaryRowTitle}>GRAND TOTAL</p>
-                    <p className={styles.summaryRowValue}>{grandTotal}</p>
+                    <p className={styles.summaryRowValue}>{numberFormatINRCurrency(grandTotal)}</p>
                 </div>
             </div>
         </>
